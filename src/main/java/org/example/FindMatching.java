7e7aa281f7e996531aa5e401411ad8442ab281ff
Zuzana Tománková <Tomankova.Z@seznam.cz>
2024-03-24 09:31:42 +0100

    Domácí úkol lekce 10

--- src/main/java/org/example/FindMatching.java
+++ src/main/java/org/example/FindMatching.java
@@ -1,2 +1,18 @@
-package org.example;public class FindMatching {
+package org.example;
+
+import java.util.List;
+
+public class FindMatching {
+    public static Double findMatching(List<Double> list, Double lowerLimit, Double upperLimit){
+
+        for (Double item:list){
+            if (item >= lowerLimit&&item<=upperLimit){
+                return item;}
+        }
+        return null;
+
+    }
+
+
+
 }
--- src/test/java/FindMatchingTest.java
+++ src/test/java/FindMatchingTest.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class FindMatchingTest {
+import org.example.FindMatching;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class FindMatchingTest {
+
+    @Test
+    public void findMatching(){
+        Double testResult = FindMatching.findMatching(List.of(12.0,8.126,6.0), 11.0, 12.6);
+        Double expected = 12.0;
+
+        Assertions.assertEquals(expected,testResult);
+    }
+
+    @Test
+    public void findMatching2(){
+        Double testResult = FindMatching.findMatching(List.of(12.0,8.126,9.0,158.0,6.0), 8.0, 10.0);
+        Double expected = 8.126;
+
+        Assertions.assertEquals(expected,testResult);
+    }
+
+
+
+
 }
